23592c26976afa194a5d6368bfb0d703ff573a9e
Khoa Do <happyboy0412@gmail.com>
2023-03-01 15:29:50 +0700

    tailwind sass autoprefixer daisyui typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    // https://docs.astro.build/en/guides/integrations-guide/tailwind/
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
+})
