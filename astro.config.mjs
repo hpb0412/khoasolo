44fdc0c0a6adf2edf44a7da873bbf213bcdfcad8
Khoa Do <happyboy0412@gmail.com>
2023-03-23 18:47:39 +0700

    feat: remove trailing splash

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://khoasolo.com',
+  trailingSlash: 'never',
   integrations: [
     // https://docs.astro.build/en/guides/integrations-guide/tailwind/
     tailwind({
