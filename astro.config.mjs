e42df43c7b6cd6f603123793b6e15a47655a6c00
Khoa Do <happyboy0412@gmail.com>
2023-04-10 09:41:35 +0700

    feat: manual sitemap lastmod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import lastmod from './lastmod.json'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
@@ -7,9 +8,11 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import robotsTxt from 'astro-robots-txt'
 
+const DOMAIN = 'https://khoasolo.com'
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://khoasolo.com',
+  site: DOMAIN,
   /* trailingSlash: 'never', */
   integrations: [
     // https://docs.astro.build/en/guides/integrations-guide/tailwind/
@@ -18,7 +21,20 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    sitemap(),
+    sitemap({
+      priority: 0.7,
+      lastmod: new Date(),
+      serialize(item) {
+        const key = item.url.replace(DOMAIN, '')
+        const foundDate = lastmod[key]
+
+        if (foundDate) {
+          item.lastmod = new Date(foundDate)
+        }
+
+        return item
+      },
+    }),
     robotsTxt({
       host: true,
     }),
